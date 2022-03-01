4d90a82cc434bf0bdf26beb12d2709f98abe6296
alan-ramon <alan.rafael2002@hotmail.com>
2022-03-01 16:18:52 -0300

    Integrando ao banco de dados

--- project/settings.py
+++ project/settings.py
@@ -79,10 +79,14 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cadastro',
+        'USER': os.getenv('USER'),
+        'PASSWORD': os.getenv('PASSWORD'),
+        'HOST': 'localhost'
     }
 }
 
